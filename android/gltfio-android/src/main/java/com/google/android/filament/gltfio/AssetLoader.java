e9238a6cafff98503e04ed0e8d76e58b80f09111
Balaji M <m.balajee@yahoo.com>
2024-06-17 22:54:16 -0400

    Revert "Include the desktop fix, for incorrect model appearance, in android (#6)"
    This reverts commit 5ea87711a9a3605af020ce4b56b34452d84e7c98.

--- android/gltfio-android/src/main/java/com/google/android/filament/gltfio/AssetLoader.java
+++ android/gltfio-android/src/main/java/com/google/android/filament/gltfio/AssetLoader.java
@@ -103,25 +103,6 @@ public class AssetLoader {
         mMaterialCache = provider;
     }
 
-    public AssetLoader(@NonNull Engine engine, @NonNull MaterialProvider provider,
-                       @NonNull EntityManager entities, String filePath) {
-
-        long nativeEngine = engine.getNativeObject();
-        long nativeEntities = entities.getNativeObject();
-        if (filePath == null) {
-            mNativeObject = nCreateAssetLoader(nativeEngine, provider, nativeEntities);
-        } else {
-            mNativeObject = nCreateAssetLoaderExtended(nativeEngine, provider, nativeEntities, filePath);
-        }
-
-        if (mNativeObject == 0) {
-            throw new IllegalStateException("Unable to parse glTF asset.");
-        }
-
-        mEngine = engine;
-        mMaterialCache = provider;
-    }
-
     /**
      * Frees all memory consumed by the native <code>AssetLoader</code>
      *
@@ -209,8 +190,6 @@ public class AssetLoader {
 
     private static native long nCreateAssetLoader(long nativeEngine, Object provider,
             long nativeEntities);
-    private static native long nCreateAssetLoaderExtended(long nativeEngine, Object provider,
-                                                          long nativeEntities, String filePath);
     private static native void nDestroyAssetLoader(long nativeLoader);
     private static native long nCreateAsset(long nativeLoader, Buffer buffer, int remaining);
     private static native long nCreateInstancedAsset(long nativeLoader, Buffer buffer, int remaining,
