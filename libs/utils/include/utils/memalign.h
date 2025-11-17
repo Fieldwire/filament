4256f77e488c0039de8577c998e085f814e70417
Balaji M <bmurali@fieldwire.com>
2025-11-16 19:37:57 -0500

    macos sequioa changes

--- libs/utils/include/utils/memalign.h
+++ libs/utils/include/utils/memalign.h
@@ -74,7 +74,7 @@ public:
     using reference = TYPE&;
     using const_reference = const TYPE&;
     using size_type = std::size_t;
-    using difference_type = std::ptrdiff_t;
+    using difference_type = ptrdiff_t;
     using propagate_on_container_move_assignment = std::true_type;
     using is_always_equal = std::true_type;
 
